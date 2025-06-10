c252bc6633b13b3023f7ce46d2446a386876e97f
Oh YoungJe <pine7420@naver.com>
2025-06-11 00:18:53 +0900

    [ICC-82] 불필요 옵션 제거

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => {
         "/api": {
           target: proxyTarget,
           changeOrigin: true,
-          secure: false,
           rewrite: (path) => path.replace(/^\/api/, ""),
           configure: (proxy) => {
             proxy.on("proxyReq", (proxyReq) => {
